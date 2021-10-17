da5de2ab77a4cccd5991e1c4b42dfb7fb6d4b395
Ma3tin <martin.mares@educanet.cz>
2021-10-17 23:33:50 +0200

    bonus

--- src/main/kotlin/Bonus.kt
+++ src/main/kotlin/Bonus.kt
@@ -3,17 +3,23 @@ import java.util.*
 fun main() {
     val sc = Scanner(System.`in`)
     val size = sc.nextLine().toInt()
-    var numbers = Array<Int>(size){0}
+    val numbers = Array<Int>(size){0}
     for (i in 0 until size) {
         numbers[i] = sc.nextLine().toInt()
     }
 
+    var counter = 1
     for (i in 0 until size) {
         for (j in i + 1 until size) {
             if (numbers[i] == numbers[j]) {
-                println(numbers[j])
+                if (counter == 1 && numbers[j] != 0) {
+                    print(numbers[i])
+                }
+                numbers[j] = 0
+                counter++
             }
         }
+        counter = 1
     }
 }
 
